37632662ec6c831615ad8864ee5a705f0429dd7c
tferrerITBA <tferrer@itba.edu.ar>
2019-06-07 21:43:38 -0300

    Fix unit test configuration

--- persistence/src/test/java/ar/edu/itba/paw/persistence/TestConfig.java
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/TestConfig.java
@@ -1,13 +1,19 @@
 package ar.edu.itba.paw.persistence;
 
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.hsqldb.jdbc.JDBCDriver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @ComponentScan({ "ar.edu.itba.paw.persistence" })
@@ -26,8 +32,22 @@ public class TestConfig {
 	}
 
 	@Bean
-	public PlatformTransactionManager transactionManager(final DataSource ds) {
-		return new DataSourceTransactionManager(ds);
+	public PlatformTransactionManager transactionManager(final EntityManagerFactory emf) {
+		return new JpaTransactionManager(emf);
+	}
+	
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		final LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+		factoryBean.setPackagesToScan("ar.edu.itba.paw.model");
+		factoryBean.setDataSource(dataSource());
+		final JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		factoryBean.setJpaVendorAdapter(vendorAdapter);
+		final Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+		factoryBean.setJpaProperties(properties);
+		return factoryBean;
 	}
 	
 }
