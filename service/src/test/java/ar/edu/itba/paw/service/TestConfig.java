7df58f485ee9504087ccd044b224992cbb46e733
tferrerITBA <tferrer@itba.edu.ar>
2019-06-13 16:57:49 -0300

    Creates first Service unit tests

---
+++ service/src/test/java/ar/edu/itba/paw/service/TestConfig.java
@@ -0,0 +1,10 @@
+package ar.edu.itba.paw.service;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({ "ar.edu.itba.paw.service" })
+public class TestConfig {
+
+}
