aa76ffc661c3c582d90e8f36009e8e0f6d38883b
tferrerITBA <tferrer@itba.edu.ar>
2019-06-18 03:16:50 -0300

    Comments SQL printing and logs to WARN in production

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -100,8 +100,8 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		final Properties properties = new Properties();
 		properties.setProperty("hibernate.hbm2ddl.auto", "update");
 		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL92Dialect");
-		properties.setProperty("hibernate.show_sql", "true");
-		properties.setProperty("format_sql", "true");
+//		properties.setProperty("hibernate.show_sql", "true");
+//		properties.setProperty("format_sql", "true");
 		factoryBean.setJpaProperties(properties);
 		return factoryBean;
 	}
