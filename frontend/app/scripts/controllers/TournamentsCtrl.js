c89a15bbef0cb3f0a6f23143cb35612d29d7021f
Santiago Swinnen <sswinnen@itba.edu.ar>
2019-12-07 20:23:19 +0100

    Pagination controller

--- frontend/app/scripts/controllers/TournamentsCtrl.js
+++ frontend/app/scripts/controllers/TournamentsCtrl.js
@@ -3,14 +3,58 @@ define(['frontend'], function(frontend) {
 
 	frontend.controller('ClubsCtrl', function($scope) {
     $scope.tournaments = [];
-    $scope.tournamentQty = 1;
-    $scope.pageInitialIndex = 1;
-    $scope.pageNum = 2;
-    $scope.totalTournamentQty = 12;
+    $scope.tournamentQty = 0;
+    $scope.pageInitialIndex = 0;
+    $scope.pageNum = 0;
+    $scope.totalTournamentQty = 0;
     $scope.now = new Date();
 
     $scope.goToTournament = function(id) {
       $location.url('tournament/' + id);
+    };
+
+    $scope.getFirstPage = function() {
+      params.pageNum = 1;
+      restService.getTournaments(params).then(function(data) {
+        $scope.tournaments = data.tournaments;
+        $scope.tournamentQty = data.tournamentQty;
+        $scope.lastPageNum = data.pagesCountMatching;
+        $scope.initialPageIndex = data.initialPageIndex;
+        $scope.pageNum = params.pageNum;
+      });
+    };
+
+    $scope.getPrevPage = function() {
+      params.pageNum--;
+      restService.getTournaments(params).then(function(data) {
+        $scope.tournaments = data.tournaments;
+        $scope.tournamentCount = data.totalTournamentsMatching;
+        $scope.lastPageNum = data.pagesCountMatching;
+        $scope.initialPageIndex = data.initialPageIndex;
+        $scope.pageNum = params.pageNum;
+      });
+    };
+
+    $scope.getNextPage = function() {
+      params.pageNum++;
+      restService.getClubs(params).then(function(data) {
+        $scope.tournaments = data.clubs;
+        $scope.tournamentsCount = data.totalTournamentsMatching;
+        $scope.lastPageNum = data.pagesCountMatching;
+        $scope.initialPageIndex = data.initialPageIndex;
+        $scope.pageNum = params.pageNum;
+      });
+    };
+
+    $scope.getLastPage = function() {
+      params.pageNum = $scope.lastPageNum;
+      restService.getClubs(params).then(function(data) {
+        $scope.tournaments = data.clubs;
+        $scope.tournamentsCount = data.totalTournamentsMatching;
+        $scope.lastPageNum = data.pagesCountMatching;
+        $scope.initialPageIndex = data.initialPageIndex;
+        $scope.pageNum = params.pageNum;
+      });
     };
 
 	});
