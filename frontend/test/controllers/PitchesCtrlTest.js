e6df878e213c603e2fbc88edbf9a389a2e91447e
Santiago Swinnen <sswinnen@itba.edu.ar>
2019-12-13 21:51:32 +0100

    Update

--- frontend/test/controllers/PitchesCtrlTest.js
+++ frontend/test/controllers/PitchesCtrlTest.js
@@ -1,21 +1,21 @@
-define(['controllers/ClubsCtrl', 'angular-mocks'], function() {
-  describe('Clubs Controller', function () {
+define(['controllers/PitchesCtrl', 'angular-mocks'], function() {
+  describe('Pitches Controller', function () {
     var controller, scope;
 
-    var TEST_CLUB_ARRAY =
+    var TEST_PITCH_ARRAY =
       [{
-        clubid: 1,
+        pitchid: 1,
         createdAt: "2019-12-12T14:55:18.752Z",
         location: "Buenos Aires",
         name: "Argentina"
       },
       {
-        clubid: 2,
+        pitchid: 2,
         createdAt: "2019-13-12T14:55:18.752Z",
         location: "Buenos Aires",
-        name: "Club"
+        name: "Pitch"
       }];
-    var TEST_CLUB_COUNT = 2;
+    var TEST_PITCH_COUNT = 2;
     var TEST_LAST_PAGE_NUM = 1;
     var TEST_INITIAL_PAGE_INDEX = 1;
     var TEST_PAGE_NUM = 1;
@@ -24,26 +24,26 @@ define(['controllers/ClubsCtrl', 'angular-mocks'], function() {
 
     beforeEach(inject(function($injector, $rootScope, $controller) {
       scope = {
-        clubs: TEST_CLUB_ARRAY,
-        clubCount: TEST_CLUB_COUNT,
+        pitches: TEST_PITCH_ARRAY,
+        pitchCount: TEST_PITCH_COUNT,
         lastPageNum:TEST_LAST_PAGE_NUM,
         initialPageIndex: TEST_INITIAL_PAGE_INDEX,
         pageNum: TEST_PAGE_NUM
       };
-      controller = $controller('ClubsCtrl', {$scope: scope});
+      controller = $controller('PitchesCtrl', {$scope: scope});
     }));
 
-    describe('$scope.clubs', function () {
+    describe('$scope.pitches', function () {
       it('should be defined', function () {
-        expect(scope.clubs).toBeDefined();
+        expect(scope.pitches).toBeDefined();
       });
 
-      it('should contain two clubs ', function () {
-        expect(scope.clubs.length).toBe(2);
+      it('should contain two pitches ', function () {
+        expect(scope.pitches.length).toBe(2);
       });
 
       it('should not have duplicate ids ', function () {
-        const ids = scope.clubs.map(function (club) {return club.clubid})
+        const ids = scope.pitches.map(function (pitch) {return pitch.pitchid})
         var isDuplicate = ids.some(function(item, idx){
           return ids.indexOf(item) !== idx
         });
@@ -52,19 +52,19 @@ define(['controllers/ClubsCtrl', 'angular-mocks'], function() {
       });
 
       it('should have the field location defined', function () {
-        scope.clubs.forEach(function (club) {
-          expect(club.location).toBeDefined();
+        scope.pitches.forEach(function (pitch) {
+          expect(pitch.location).toBeDefined();
         })
       });
     });
 
-    describe('$scope.clubCount', function () {
+    describe('$scope.pitchCount', function () {
       it('should be defined', function () {
-        expect(scope.clubCount).toBeDefined();
+        expect(scope.pitchCount).toBeDefined();
       });
 
       it('should equal 2', function () {
-        expect(scope.clubCount).toBe(2);
+        expect(scope.pitchCount).toBe(2);
       });
 
     });
