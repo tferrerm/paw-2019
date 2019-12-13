d5a15d9af5010998df11ed0994665790e5784575
Santiago Swinnen <sswinnen@itba.edu.ar>
2019-12-13 21:36:29 +0100

    Tournaments tests added and passing

--- frontend/test/controllers/HomeCtrlTest.js
+++ frontend/test/controllers/HomeCtrlTest.js
@@ -29,5 +29,5 @@ define(['controllers/HomeCtrl', 'angular-mocks'], function() {
       });
 
     });
-  })
-})
+  });
+});
--- frontend/test/controllers/TournamentsCtrlTest.js
+++ frontend/test/controllers/TournamentsCtrlTest.js
@@ -1,21 +1,19 @@
-define(['controllers/ClubsCtrl', 'angular-mocks'], function() {
-  describe('Clubs Controller', function () {
+define(['controllers/TournamentsCtrl', 'angular-mocks'], function() {
+  describe('Tournaments Controller', function () {
     var controller, scope;
 
-    var TEST_CLUB_ARRAY =
+    var TEST_TOURNAMENT_ARRAY =
       [{
-        clubid: 1,
+        tournamentid: 1,
         createdAt: "2019-12-12T14:55:18.752Z",
-        location: "Buenos Aires",
         name: "Argentina"
       },
       {
-        clubid: 2,
+        tournamentid: 2,
         createdAt: "2019-13-12T14:55:18.752Z",
-        location: "Buenos Aires",
-        name: "Club"
+        name: "Tournament"
       }];
-    var TEST_CLUB_COUNT = 2;
+    var TEST_TOURNAMENT_COUNT = 2;
     var TEST_LAST_PAGE_NUM = 1;
     var TEST_INITIAL_PAGE_INDEX = 1;
     var TEST_PAGE_NUM = 1;
@@ -24,26 +22,26 @@ define(['controllers/ClubsCtrl', 'angular-mocks'], function() {
 
     beforeEach(inject(function($injector, $rootScope, $controller) {
       scope = {
-        clubs: TEST_CLUB_ARRAY,
-        clubCount: TEST_CLUB_COUNT,
+        tournaments: TEST_TOURNAMENT_ARRAY,
+        tournamentCount: TEST_TOURNAMENT_COUNT,
         lastPageNum:TEST_LAST_PAGE_NUM,
         initialPageIndex: TEST_INITIAL_PAGE_INDEX,
         pageNum: TEST_PAGE_NUM
       };
-      controller = $controller('ClubsCtrl', {$scope: scope});
+      controller = $controller('TournamentsCtrl', {$scope: scope});
     }));
 
-    describe('$scope.clubs', function () {
+    describe('$scope.tournaments', function () {
       it('should be defined', function () {
-        expect(scope.clubs).toBeDefined();
+        expect(scope.tournaments).toBeDefined();
       });
 
-      it('should contain two clubs ', function () {
-        expect(scope.clubs.length).toBe(2);
+      it('should contain two tournaments ', function () {
+        expect(scope.tournaments.length).toBe(2);
       });
 
       it('should not have duplicate ids ', function () {
-        const ids = scope.clubs.map(function (club) {return club.clubid})
+        const ids = scope.tournaments.map(function (tournament) {return tournament.tournamentid})
         var isDuplicate = ids.some(function(item, idx){
           return ids.indexOf(item) !== idx
         });
@@ -51,20 +49,15 @@ define(['controllers/ClubsCtrl', 'angular-mocks'], function() {
         expect(isDuplicate).toBe(false);
       });
 
-      it('should have the field location defined', function () {
-        scope.clubs.forEach(function (club) {
-          expect(club.location).toBeDefined();
-        })
-      });
     });
 
-    describe('$scope.clubCount', function () {
+    describe('$scope.tournamentCount', function () {
       it('should be defined', function () {
-        expect(scope.clubCount).toBeDefined();
+        expect(scope.tournamentCount).toBeDefined();
       });
 
       it('should equal 2', function () {
-        expect(scope.clubCount).toBe(2);
+        expect(scope.tournamentCount).toBe(2);
       });
 
     });
