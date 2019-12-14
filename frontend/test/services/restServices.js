cef1c043652b25fef70489643393f4bbbaaa6a3f
Santiago Swinnen <sswinnen@itba.edu.ar>
2019-12-14 11:54:24 +0100

    Added rest services tests for tournaments

--- frontend/test/services/restServices.js
+++ frontend/test/services/restServices.js
@@ -4,6 +4,7 @@ define(['services/restService', 'angular-mocks'], function() {
     var restService, url, http, $q;
 
     var TEST_EVENTS = [{eventid: 1, name: "Evento 1"}, {eventid: 2, name: "Evento 2"}];
+    var TEST_TOURNAMENTS = [{tournamentid: 1, name: "Tournament 1"}, {tournamentid: 2, name: 'Tournamnet 2'}]
 
     beforeEach(module('frontend'));
 
@@ -42,7 +43,34 @@ define(['services/restService', 'angular-mocks'], function() {
         expect(response).toEqual(TEST_EVENTS[0]);
       });
     });
+
+    describe('.getTournaments()', function() {
+      var response;
+
+      it('should be defined', function() {
+        expect(restService.getTournaments).toBeDefined();
+      });
+
+      it('should return a valid list of tournaments (no params)', function() {
+        http.whenGET(url + '/tournaments').respond(200, $q.when(TEST_TOURNAMENTS));
+
+        restService.getTournaments().then(function(res) {response = res;});
+
+        http.flush();
+        expect(response).toEqual(TEST_TOURNAMENTS);
+      });
+
+      it('should return a valid list of tournaments (with params)', function() {
+        http.whenGET(url + '/tournaments?pageNum=1').respond(200, $q.when(TEST_TOURNAMENTS[0]));
+
+        restService.getTournaments({pageNum: 1}).then(function(resp) {response = resp});
+        http.flush();
+        expect(response).toEqual(TEST_TOURNAMENTS[0]);
+      });
+    });
   })
+
+
 
 
 })
