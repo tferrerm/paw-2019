e1446fc09bba95db148683639309c8d2815cbe79
Santiago Swinnen <sswinnen@itba.edu.ar>
2019-12-14 11:59:24 +0100

    Added rest services for pitches

--- frontend/test/services/restServices.js
+++ frontend/test/services/restServices.js
@@ -5,6 +5,7 @@ define(['services/restService', 'angular-mocks'], function() {
 
     var TEST_EVENTS = [{eventid: 1, name: "Evento 1"}, {eventid: 2, name: "Evento 2"}];
     var TEST_TOURNAMENTS = [{tournamentid: 1, name: "Tournament 1"}, {tournamentid: 2, name: 'Tournamnet 2'}]
+    var TEST_PITCHES = [{pitchid: 1, name: "Pitch 1"}, {pitchid: 2, name: 'Pitch 2'}]
 
     beforeEach(module('frontend'));
 
@@ -68,9 +69,30 @@ define(['services/restService', 'angular-mocks'], function() {
         expect(response).toEqual(TEST_TOURNAMENTS[0]);
       });
     });
+
+    describe('.getPitches()', function() {
+      var response;
+
+      it('should be defined', function() {
+        expect(restService.getPitches).toBeDefined();
+      });
+
+      it('should return a valid list of pitches (no params)', function() {
+        http.whenGET(url + '/pitches').respond(200, $q.when(TEST_PITCHES));
+
+        restService.getPitches().then(function(res) {response = res;});
+
+        http.flush();
+        expect(response).toEqual(TEST_PITCHES);
+      });
+
+      it('should return a valid list of pitches (with params)', function() {
+        http.whenGET(url + '/pitches?pageNum=1').respond(200, $q.when(TEST_PITCHES[0]));
+
+        restService.getPitches({pageNum: 1}).then(function(resp) {response = resp});
+        http.flush();
+        expect(response).toEqual(TEST_PITCHES[0]);
+      });
+    });
   })
-
-
-
-
-})
+});
